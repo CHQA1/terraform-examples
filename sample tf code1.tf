b8beca7cf18a08e0b43ca9c32e7433860b34f194
sureshpackiam <78549408+sureshpackiam@users.noreply.github.com>
2022-03-18 09:42:22 +0000

    Update sample tf code1.tf

--- sample tf code1.tf
+++ sample tf code1.tf
@@ -2,6 +2,7 @@ module aws_reverse_proxy {
   # Available inputs httpsgithub.comfuturiceterraform-utilstreemasteraws_reverse_proxy#inputs
   # Check for updates httpsgithub.comfuturiceterraform-utilscomparev11.0...master
   # extra comment line edited again to check webhook
+  # extra comment line added for testing branch commits
   source = gitsshgit@github.comfuturiceterraform-utils.gitaws_reverse_proxyref=v11.0
 
   origin_url             = httpexample.com           # note that this is just a dummy value to satisfy CloudFront, it won't ever be used with the override_ variables in place
